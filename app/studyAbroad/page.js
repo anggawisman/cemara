9fad9179ea08744b215c13e3549b5f4cc0a47c4c
anggawisman <angga.wisman.nhf@gmail.com>
2024-05-19 20:24:15 +0700

    gangerti

--- app/studyAbroad/page.js
+++ app/studyAbroad/page.js
@@ -1,6 +1,6 @@
 // import React from "react";
 import Image from "next/image";
-import Article from "@/components/Article";
+import Article from "../../components/Article";
 import { STUDY_ABROAD } from "@/Constants";
 
 const StudyAbroad = () => {
