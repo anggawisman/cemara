03ba39349a059831c0e9922af19a7fc14f437132
anggawisman <101158833+anggawisman@users.noreply.github.com>
2024-08-07 20:31:06 +0700

    add GTM

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
 import Navbar from "@/layouts/Navbar";
 import Footer from "@/layouts/Footer";
 
@@ -13,6 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-5T2D64MG" />
       <body className="">
         <Navbar />
         <main className=" relative overflow-hidden">{children}</main>
